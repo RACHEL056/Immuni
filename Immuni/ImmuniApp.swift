f89abf0fd49df50e53f9760b3b8857362a5c8fc8
박민정 <135407041+RACHEL056@users.noreply.github.com>
2024-04-15 19:27:40 +0900

    Add files via upload

---
+++ Immuni/ImmuniApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImmuniApp.swift
+//  Immuni
+//
+//  Created by 박민정 on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ImmuniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
